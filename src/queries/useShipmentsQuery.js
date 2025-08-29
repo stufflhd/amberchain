1988096cd921101a931be92b45bf37fe4274f4d6
stufflhd <stufflhd@gmail.com>
2025-08-29 16:09:51 +0100

    add comment to useShipmentsQuery

--- src/queries/useShipmentsQuery.js
+++ src/queries/useShipmentsQuery.js
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchAllShipments } from '@/services/apiService';
 
+// query shipements data
 export const useShipmentsQuery = () => {
     return useQuery({
         queryKey: ['shipments'],
