fcfe211cf7292b6f049ee13546df5cb08b95e466
stufflhd <stufflhd@gmail.com>
2025-10-10 10:13:25 +0100

    Ignore Auth-guide and swagger.json

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -19,7 +19,7 @@ export default function LanguageSelector() {
                 className="rounded-full [&>svg]:text-muted-foreground/80 hover:bg-accent hover:text-accent-foreground px-2 max-h-8 shadow-none [&>svg]:shrink-0"
                 aria-label="Select language">
                 <GlobeIcon size={16} aria-hidden="true" />
-                <SelectValue className="hidden sm:inline-flex h-min" />
+                <SelectValue className="hidden sm:inline-flex h-min" placeholder=""/>
             </SelectTrigger>
             <SelectContent
                 className="[&_*[role=option]]:ps-2 [&_*[role=option]]:pe-8 [&_*[role=option]>span]:start-auto [&_*[role=option]>span]:end-2 [&_*[role=option]>span]:flex [&_*[role=option]>span]:items-center [&_*[role=option]>span]:gap-2">
